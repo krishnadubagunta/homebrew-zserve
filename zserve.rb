b335c9afeadada13f242209f12985bc4cdb5c4f0
krishnadubagunta <dubagunta.saikrishna@outlook.com>
2025-10-05 10:53:07 -0500

    release: changed URL for download to archive

--- zserve.rb
+++ zserve.rb
@@ -1,7 +1,7 @@
 class Zserve < Formula
   desc "Lightweight Zig-based HTTP shell server"
   homepage "https://github.com/krishnadubagunta/zserve"
-  url "https://github.com/krishnadubagunta/zserve/releases/download/1.0.0/zserve-1.0.0.tar.gz"
+  url "https://github.com/krishnadubagunta/zserve/archive/refs/tags/1.0.0.tar.gz"
   sha256 "7bd4ba20fa11c616f5ed95bdbc8e3538541718e8e78fb995677bfb4fc8e4c341"
   license "MIT"
 
