cf0c81e0b0deafa79492515aa274f2aff8b9a44e
krishnadubagunta <dubagunta.saikrishna@outlook.com>
2025-10-05 10:48:27 -0500

    release: first brew

---
+++ zserve.rb
@@ -0,0 +1,15 @@
+class Zserve < Formula
+  desc "Lightweight Zig-based HTTP shell server"
+  homepage "https://github.com/krishnadubagunta/zserve"
+  url "https://github.com/krishnadubagunta/zserve/releases/download/v0.1.0/zserve-0.1.0.tar.gz"
+  sha256 "7bd4ba20fa11c616f5ed95bdbc8e3538541718e8e78fb995677bfb4fc8e4c341"
+  license "MIT"
+
+  def install
+    bin.install "zserve"
+  end
+
+  test do
+    system "#{bin}/zserve", "--version"
+  end
+end
