de1a5dedeebd88b422804de0e1ba3350faa8eecb
krishnadubagunta <dubagunta.saikrishna@outlook.com>
2025-10-05 22:28:27 -0500

    release: zigsh to zserve mismatch fix

--- zserve.rb
+++ zserve.rb
@@ -1,7 +1,7 @@
 class Zserve < Formula
   desc "Lightweight Zig-based HTTP shell server"
   homepage "https://github.com/krishnadubagunta/zserve"
-  url "https://github.com/krishnadubagunta/zigsh/releases/download/v1.0.1/zserve-1.0.1.tar.gz"
+  url "https://github.com/krishnadubagunta/zserve/releases/download/v1.0.1/zserve-1.0.1.tar.gz"
   sha256 "712572f003bd660b5c5f74fee27cf22012a630207c2ccb51c1516f085fc95c79"
   license "MIT"
 
