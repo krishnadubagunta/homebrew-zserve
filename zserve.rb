956403809dca31ded3b8b740126f4fa54ce94e29
krishnadubagunta <dubagunta.saikrishna@outlook.com>
2025-10-05 22:29:09 -0500

    release: sha mismatch fix

--- zserve.rb
+++ zserve.rb
@@ -2,7 +2,7 @@ class Zserve < Formula
   desc "Lightweight Zig-based HTTP shell server"
   homepage "https://github.com/krishnadubagunta/zserve"
   url "https://github.com/krishnadubagunta/zserve/releases/download/v1.0.1/zserve-1.0.1.tar.gz"
-  sha256 "712572f003bd660b5c5f74fee27cf22012a630207c2ccb51c1516f085fc95c79"
+  sha256 "c79f06ecfb6ca7a0529747cd34be9d4822c3c0d37799ea864d8599e085ff1980"
   license "MIT"
 
   def install
